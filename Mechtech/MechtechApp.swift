168f707e195c8de4d75a495bb5082c7382b8980a
nihalpothunoori <npothunoori@gmail.com>
2022-11-04 18:16:40 -0400

    Initial Commit

---
+++ Mechtech/MechtechApp.swift
@@ -0,0 +1,17 @@
+//
+//  MechtechApp.swift
+//  Mechtech
+//
+//  Created by Nihal Pothunoori on 11/4/22.
+//
+
+import SwiftUI
+
+@main
+struct MechtechApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
